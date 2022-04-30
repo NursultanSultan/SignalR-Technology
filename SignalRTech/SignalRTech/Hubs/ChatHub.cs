d58e6294b7dfcb4b99f1026cae7dc825fab2b250
NursultanSultan <tu201906251@code.edu.az>
2022-04-30 11:35:17 +0400

    feat: first commit

---
+++ SignalRTech/SignalRTech/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRTech.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageAsync(string message)
+        {
+            await Clients.All.SendAsync("receiveMessage", message);
+        }
+    }
+}
