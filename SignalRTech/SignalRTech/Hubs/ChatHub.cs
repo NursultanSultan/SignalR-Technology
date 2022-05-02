e4707c76120ae2ac3f7879c54ef3201c84cbe175
NursultanSultan <tu201906251@code.edu.az>
2022-05-03 01:04:52 +0400

    feat: override connection-event methods OnConnectedAsync and OnDisconnectedAsync

--- SignalRTech/SignalRTech/Hubs/ChatHub.cs
+++ SignalRTech/SignalRTech/Hubs/ChatHub.cs
@@ -12,5 +12,26 @@ namespace SignalRTech.Hubs
         {
             await Clients.All.SendAsync("receiveMessage", message);
         }
+ 
+        /// <summary>
+        /// Sisteme (Hub- a)  bir client daxil olduqda bu method tetiklenir
+        /// </summary>
+        /// <returns></returns>
+        public override async Task OnConnectedAsync()
+        {
+            var ConnectionId = Context.ConnectionId;
+            await Clients.All.SendAsync("userJoined", ConnectionId);
+        }
+
+        /// <summary>
+        /// Sistemden (Hub- dan) bir client baqlantisini qopardiqda bu method tetiklenir
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            var ConnectionId = Context.ConnectionId;
+            await Clients.All.SendAsync("userLeaved", ConnectionId);
+        }
     }
 }
