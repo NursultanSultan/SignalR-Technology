16921dd7375baac75428a2aa1a71380ac53a2dde
NursultanSultan <tu201906251@code.edu.az>
2022-05-03 02:10:46 +0400

    feat: list all connected clients

--- SignalRTech/SignalRTech/Hubs/ChatHub.cs
+++ SignalRTech/SignalRTech/Hubs/ChatHub.cs
@@ -8,6 +8,11 @@ namespace SignalRTech.Hubs
 {
     public class ChatHub : Hub
     {
+        /// <summary>
+        /// Butun clients ozunde saxlayan list
+        /// </summary>
+        public static List<string> clients = new List<string>();
+
         public async Task SendMessageAsync(string message)
         {
             await Clients.All.SendAsync("receiveMessage", message);
@@ -20,6 +25,8 @@ namespace SignalRTech.Hubs
         public override async Task OnConnectedAsync()
         {
             var ConnectionId = Context.ConnectionId;
+            clients.Add(ConnectionId);
+            await Clients.All.SendAsync("clients", clients); // Clients list client-side da goruntulemey ucun
             await Clients.All.SendAsync("userJoined", ConnectionId);
         }
 
@@ -31,6 +38,8 @@ namespace SignalRTech.Hubs
         public override async Task OnDisconnectedAsync(Exception exception)
         {
             var ConnectionId = Context.ConnectionId;
+            clients.Remove(ConnectionId);
+            await Clients.All.SendAsync("clients", clients); // Clients list client-side da goruntulemey ucun
             await Clients.All.SendAsync("userLeaved", ConnectionId);
         }
     }
